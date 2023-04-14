6d2ea54f835d6f0eec84f368165dc2ce70a7e2f2
federico <fedeasaad1099@outlook.com.ar>
2023-04-13 22:24:27 -0300

    cambios fede

--- client/src/components/SectionCategories/SectionCategories.jsx
+++ client/src/components/SectionCategories/SectionCategories.jsx
@@ -17,14 +17,13 @@ const SectionCategories = () => {
       <div className={styles.containerProducts}>
         {allProducts?.map((product, i) => {
           return (
-            <>
               <ProductCard
                 key={i}
                 name={product.name}
                 price={product.price}
                 size={product.size}
+                imagen={product.imagen}
               />
-            </>
           );
         })}
       </div>
