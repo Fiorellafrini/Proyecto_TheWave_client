412d661d71af1ff6e7d0f05733e3e648b61e258f
henrydjpacheco <henrydjpacheco@gmailcom>
2023-04-14 19:55:34 -0500

    prueba

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -124,27 +124,4 @@ export function filterByPriceDesc() {
 
 
 
-export function listProducts(page) {
-    return async function (dispatch) {
-        const json = await axios.get(`http://localhost:3001/product?page=${page}&size=5`);
-        return dispatch({
-        type: "GET_ALL_PRODUCTS",
-        payload: json.data.products,
-        });
-    };
-};
 
-export const listTypes = () => async (dispatch) => {
-  const { data } = await axios("http://localhost:3001/type");
-  return dispatch({
-    type: "GET_All_TYPES",
-    payload: data,
-  });
-};
-export const listBrands = () => async (dispatch) => {
-  const { data } = await axios("http://localhost:3001/brand");
-  return dispatch({
-    type: "GET_ALL_BRANDS",
-    payload: data,
-  });
-};
